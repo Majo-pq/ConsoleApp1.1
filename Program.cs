c79a87e44aaccb3ec742eaaee367d3cbdb98238f
Maria Jose Pecina Quintana <h21050059@monclova.tecnm.mx>
2021-10-05 17:14:54 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ukelele uke = new Ukelele("cafe", "soprano");
+
+            Console.WriteLine(uke.ToString());
+        }
+    }
+}
---
+++ Ukelele.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1._1
+{
+    class Ukelele
+    {
+        private string color;
+        private string tipo;
+
+        public string Color
+        {
+            get
+            {
+                return Color;
+            }
+            set
+            {
+                Color = value;
+            }
+        }
+
+        public string Tipo
+        {
+            get
+            {
+                return Tipo;
+            }
+            set
+            {
+                Tipo = value;
+            }
+        }
+        public Ukelele()
+        {
+            color = "";
+            tipo = "";
+        }
+        public Ukelele(string color, string tipo)
+        {
+            this.color = color;
+            this.tipo = tipo;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "color:" + color + "tipo[" + tipo.ToString() + "]";
+        }
+    }
+
+    }
+
+    
